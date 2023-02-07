d5a4cacff932f967041f28c597b3a86291299a03
kateFrontend <ecdanilova@gmail.com>
2023-02-07 12:02:17 -0500

    finish script

--- script.js
+++ script.js
@@ -33,4 +33,18 @@ function update() {
       circle.classList.remove("active");
     }
   });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
 }
